0017d1aea7d9e4b01f30c902f80e553cfa71be78
olegtaratuhin <o.taratuhin@gmail.com>
2019-01-09 22:05:28 +0100

    Changed example

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ from upgma_simple import UpgmaTrivial as upgma
 if __name__ == '__main__':
     labels = "A B C D".split(' ')
     matrix = [
-        [0, 3, 3, 3],
-        [0, 0, 2, 2],
-        [0, 0, 0, 1],
-        [0, 0, 0, 0]
+        [0, 3, 3, 3],  # A
+        [0, 0, 2, 2],  # B
+        [0, 0, 0, 1],  # C
+        [0, 0, 0, 0]   # D
     ]
 
     tree = upgma(labels, matrix).tree
